d7a54e4d08758af6f7f0246b2c0b6467214c56c2
shihanmilinda7 <shihanmilinda7@gmail.com>
2023-10-19 20:43:18 +0530

    first commit

---
+++ src/web-socket.ts
@@ -0,0 +1,8 @@
+import io from "socket.io-client";
+export let webSocket;
+if (process.env.NODE_ENV === "development") {
+  // Code to run in development environment
+  webSocket = io("http://localhost:5000");
+} else {
+  // Code to run in production environment
+}

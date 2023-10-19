b755442ed4ef08a7589716702d7a2fcea207c1ba
shihanmilinda7 <shihanmilinda7@gmail.com>
2023-10-19 20:45:49 +0530

    d

--- src/web-socket.ts
+++ src/web-socket.ts
@@ -4,5 +4,5 @@ if (process.env.NODE_ENV === "development") {
   // Code to run in development environment
   webSocket = io("http://localhost:5000");
 } else {
-  // Code to run in production environment
+  // Code to run in production environmentd
 }
